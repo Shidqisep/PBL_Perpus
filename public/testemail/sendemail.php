87342405e5e2de74e9ca59361d0a73551f3a67de
shidqisep <shidathalah@gmail.com>
2025-10-25 17:35:52 +0700

    php mailer bisa coyyyy

--- public/testemail/sendemail.php
+++ public/testemail/sendemail.php
@@ -1,26 +1,44 @@
 <?php
-require './vendor/phpmailer';
+
+require __DIR__.'/../../vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
+use Dotenv\Dotenv;
+
+
+// Ini adalah penggabungan string yang benar
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
 
 $mail = new PHPMailer(true);
+//ga bisa pake getenv
+// var_dump(getenv('SMTP_HOST'));
+// var_dump(getenv('SMTP_PORT'));
+// var_dump(getenv('SMTP_USER'));
+// var_dump(getenv('SMTP_PASS'));
+
+// var_dump($_ENV['SMTP_HOST']);
+// var_dump($_ENV['SMTP_PORT']);
+// var_dump($_ENV['SMTP_USER']);
+// var_dump($_ENV['SMTP_PASS']);
 
 try {
     $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'shidathalah@gmail.com'; // ganti email kamu
-    $mail->Password = 'your_app_password';   // gunakan App Password, bukan password Gmail biasa
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->Host = $_ENV['SMTP_HOST'];
+    $mail->Port = $_ENV['SMTP_PORT'];
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = 587;
-
-    $mail->setFrom('shidathalah@gmail.com', 'Mailer Docker');
+    $mail->SMTPAuth = true;
+    $mail->Username = $_ENV['SMTP_USER']; // ganti email kamu
+    $mail->Password = $_ENV['SMTP_PASS'];   // gunakan App Password, bukan password Gmail biasa
+    $mail->setFrom($_ENV['SMTP_USER'], 'Mailer Docker');
     $mail->addAddress('kmuradiab@gmail.com'); // email tujuan
 
-    $mail->isHTML(true);
+    $mail->isHTML(false);
     $mail->Subject = 'Test PHPMailer from Docker';
-    $mail->Body    = '<b>Berhasil!</b> Email ini dikirim lewat PHPMailer di dalam Docker.';
+    $mail->Body    = 'Berhasil! Email ini dikirim lewat PHPMailer di dalam Docker.';
     $mail->AltBody = 'Berhasil! Email ini dikirim lewat PHPMailer di dalam Docker.';
 
     $mail->send();
